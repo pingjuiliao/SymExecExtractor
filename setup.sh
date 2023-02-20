97e9f18cf186ed74f8f4b80871c029b30432751c
Ping-Jui Liao <pingjuiliao@gmail.com>
2023-02-19 17:07:02 -0800

    add gitignore and setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LLVM_BUILD_DIR=build
+
+mkdir -p ${LLVM_BUILD_DIR}
+pushd ${LLVM_BUILD_DIR}
+cmake ../llvm/llvm -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
+popd
